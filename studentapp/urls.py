cd001b97c6177aa4e991c43da67f35da659be9fb
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-24 22:53:48 +0600

    feat: add student edit and delete views to studentapp

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -3,6 +3,8 @@ from studentapp.views import *
 
 urlpatterns = [
     path("student-list/", student_list, name="student_list"),
+    path("student-list/<int:id>/", student_view, name="student_view"),
+    path("student-edit/<int:id>/", student_edit, name="student_edit"),
+    path("student-delete/<int:id>/", student_delete, name="student_delete"),
     path("student-add/", add_student, name="add_student"),
-    path("student-view/<int:id>/", student_view, name="student_view"),
 ]
--- studentapp/views.py
+++ studentapp/views.py
@@ -54,3 +54,48 @@ def student_view(request, id):
         },
         status=status.HTTP_200_OK,
     )
+
+
+@api_view(["PUT"])
+def student_edit(request, id):
+    try:
+        data = StudentInfo.objects.get(id=id)
+    except StudentInfo.DoesNotExist:
+        return Response(
+            {"success": False, "message": "Student Not Found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    if request.method == "PUT":
+        serializer = StudentSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "success": True,
+                    "message": "Student Edit Successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"Success": False, "message": "Student Edit Unsuccessfull"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+
+@api_view(["DELETE"])
+def student_delete(request, id):
+    try:
+        data = StudentInfo.objects.get(id=id)
+    except StudentInfo.DoesNotExist:
+        return Response(
+            {"success": False, "message": "Student Not Found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    data.delete()
+    return Response(
+        {
+            "Success": True,
+            "Message": "Student Data Deleted Succesfully",
+        }
+    )
