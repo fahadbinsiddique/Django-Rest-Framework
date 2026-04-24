d64952a15d22904b95554d43228cd0c420ff7ee1
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-25 01:12:27 +0600

    fix: add student views and admin registration

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from studentapp.views import *
 
-urlpatterns = [path("student-list/", student_list, name="student_list")]
+urlpatterns = [
+    path("student-list/", student_list, name="student_list"),
+    path("student-add/", add_student, name="add_student"),
+    path("student-view/<int:id>/", student_view, name="student_view"),
+]
--- studentapp/views.py
+++ studentapp/views.py
@@ -5,8 +5,52 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def student_list(request):
-    student_data= StudentInfo.objects.all()
-    serializer= StudentSerializer(student_data,many=True)
-    return Response(serializer,status=status.HTTP_200_OK)
+    student_data = StudentInfo.objects.all()
+    serializer = StudentSerializer(student_data, many=True)
+    return Response(
+        {
+            "success": True,
+            "message": "Student List Successfuly GET",
+            "data": serializer.data,
+        },
+        status=status.HTTP_200_OK,
+    )
+
+
+@api_view(["POST"])
+def add_student(request):
+    if request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "success": True,
+                    "message": "Student ADD Successfuly POST",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response({"success": False, "message": serializer.errors})
+
+
+@api_view(["GET"])
+def student_view(request, id):
+    try:
+        data = StudentInfo.objects.get(id=id)
+    except StudentInfo.DoesNotExist:
+        return Response(
+            {"success": False, "message": "Student Not Found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    serializer = StudentSerializer(data)
+    return Response(
+        {
+            "success": True,
+            "message": "Student find Successfuly GET",
+            "data": serializer.data,
+        },
+        status=status.HTTP_200_OK,
+    )
